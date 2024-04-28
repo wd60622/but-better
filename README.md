695262e5939ed647c350cf22d95ecfbd446817c2
Will Dean <wd60622@gmail.com>
2024-04-28 12:14:56 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Your Code But Better
+
+Wrapper for your code to make it nothing but better. 
+
+## Installation
+
+```bash
+pip install but-better
+```
+
+## Usage
+
+Wrap your function with `but_better` and pass the youtube video id as the argument. 
+
+```python 
+from your_module import your_function
+
+from but_better import but_better
+
+# Last bit of any youtube video url
+youtube_id = "dQw4w9WgXcQ"
+your_function_but_better = but_better(youtube_id)(your_function)
+```
+
+Use the wrapped function as you would normally. 
+
+```python
+your_function_but_better()
+```
---
+++ but_better/__init__.py
@@ -0,0 +1,50 @@
+from IPython import get_ipython
+from IPython.display import display, YouTubeVideo
+from functools import wraps
+import warnings
+
+
+def in_jupyter_notebook() -> bool:
+    return get_ipython().__class__.__name__ == "ZMQInteractiveShell"
+
+
+def but_better(video_id: str, **youtube_kwargs):
+    """Wrap any function with this decorator to play a YouTube video while it runs.
+
+    Args: 
+        video (str): The YouTube video ID.
+        **youtube_kwargs: Additional keyword arguments to pass to `IPython.display.YouTubeVideo`.
+
+    Returns:
+        function: The decorated function.
+
+    Examples: 
+        
+        @but_better("z3SEc70eQYE")
+        def phillies_hype_song():
+            print("Let's go Phillies!")
+
+
+        @but_better("mwgcK4E_RU0")
+        def favorite_customer():
+            print("You're my favorite customer!")
+
+    """
+    if not in_jupyter_notebook():
+        warnings.warn("This decorator only works in Jupyter notebooks.", UserWarning)
+        return lambda func: func
+
+    if "allow_autoplay" not in youtube_kwargs: 
+        youtube_kwargs["allow_autoplay"] = True
+
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            display(YouTubeVideo(video_id, **youtube_kwargs))
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
+
+
+phillies_hype_song = but_better("z3SEc70eQYE")
+favorite_customer = but_better("mwgcK4E_RU0")
